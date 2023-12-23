1493d9029e126baa7a756ff3529b176e264fd36f
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2023-12-23 13:33:22 -0500

    controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users,
              controllers: {
-                 sessions: 'users/sessions',
-                 registrations: 'users/registrations'
+               sessions: "users/sessions",
+               registrations: "users/registrations"
              }
-  get '/test', to: 'test#show'
+  get "/test", to: "test#show"
+  namespace :api do
+    namespace :v1 do
+      resources :members do
+        resources :facts
+      end
+    end
+  end
 end
