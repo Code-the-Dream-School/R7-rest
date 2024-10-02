5cf537e7d6a81406c54a96c4810a0e4215109d8f
lemonade <nicole.jordanncj@gmail.com>
2024-10-02 19:00:22 -0400

    REST API, JSON, Devise, Postman

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations'
+  }
+
+  get '/test', to: 'test#show'  
+end
+
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-end
